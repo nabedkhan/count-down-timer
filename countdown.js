1f9785661a07c3280c4c3ef59678500cd2734919
Nabed Khan <nabed420@gmail.com>
2020-08-07 00:19:59 +0600

    update file

--- countdown.js
+++ countdown.js
@@ -2,32 +2,27 @@ const dayTextValue = document.querySelector('.days');
 const hourTextValue = document.querySelector('.hours');
 const minuteTextValue = document.querySelector('.minutes');
 const secondTextValue = document.querySelector('.seconds');
-// ending date time
-let target_date = new Date('12/16/2020').getTime();
-let days, hours, minutes, seconds;
-let current_date = new Date().getTime();
 
-/// target time is over
-if (current_date >= target_date) {
-    console.log('Time Up!!!');
-}
-else {
-    setInterval(function () {
-        let seconds_left = (target_date - current_date) / 1000;
+/// ending time
+let target_date = new Date('10/16/2020').getTime();
 
-        days = parseInt(seconds_left / 86400);  //per day seconds is 86400
-        seconds_left = seconds_left % 86400;
 
-        hours = parseInt(seconds_left / 3600);  //per hour seconds is 3600
-        seconds_left = seconds_left % 3600;
+setInterval(function () {
+    let current_date = new Date().getTime();
+    let seconds_left = (target_date - current_date) / 1000;
 
-        minutes = parseInt(seconds_left / 60);
-        seconds = parseInt(seconds_left % 60);
+    const days = parseInt(seconds_left / 86400);  //per day seconds is 86400
+    seconds_left = seconds_left % 86400;
 
-        dayTextValue.textContent = days;
-        hourTextValue.textContent = hours;
-        minuteTextValue.textContent = minutes;
-        secondTextValue.textContent = seconds;
+    const hours = parseInt(seconds_left / 3600);  //per hour seconds is 3600
+    seconds_left = seconds_left % 3600;
 
-    }, 1000);
-}
+    const minutes = parseInt(seconds_left / 60);
+    const seconds = parseInt(seconds_left % 60);
+
+    dayTextValue.textContent = days;
+    hourTextValue.textContent = hours;
+    minuteTextValue.textContent = minutes;
+    secondTextValue.textContent = seconds;
+
+}, 1000);
